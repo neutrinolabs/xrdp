dc69341a57e3b968fcbfcdcc6479482418360dae
jsorg71 <jsorg71>
2006-02-04 18:43:21 +0000

    copyright year update

--- rdp/rdp_lic.c
+++ rdp/rdp_lic.c
@@ -14,7 +14,7 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
    xrdp: A Remote Desktop Protocol server.
-   Copyright (C) Jay Sorg 2005
+   Copyright (C) Jay Sorg 2005-2006
 
    licence
 
