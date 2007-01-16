aefa66e3a2f537baf2e952142e4554260f6c7e6f
jsorg71 <jsorg71>
2007-01-16 07:33:17 +0000

    remove old files

--- instfiles/startwm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# not used, the one in sesman directory is used
-#
-#startkde
-#blackbox
-#fvwm95
--- instfiles/xrdpstart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /usr/local/xrdp
-./xrdp > /usr/local/xrdp/xrdp.boot.log
-./sesman > /usr/local/xrdp/sesman.boot.log
