bb88a8285faa8c2a140a24c531742da128549a5a
jsorg71 <jsorg71>
2007-06-03 08:46:23 +0000

    early work

--- mc/mc.c
+++ mc/mc.c
@@ -20,6 +20,94 @@
 
 */
 
-#include "libmc.h"
+#include "mc.h"
 
 /*****************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_start(struct mod* mod, int w, int h, int bpp)
+{
+  LIB_DEBUG(mod, "in lib_mod_start");
+  mod->width = w;
+  mod->height = h;
+  mod->bpp = bpp;
+  LIB_DEBUG(mod, "out lib_mod_start");
+  return 0;
+}
+
+/******************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_connect(struct mod* mod)
+{
+  LIB_DEBUG(mod, "in lib_mod_connect");
+  LIB_DEBUG(mod, "out lib_mod_connect");
+  return 0;
+}
+
+/******************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_event(struct mod* mod, int msg, long param1, long param2,
+              long param3, long param4)
+{
+  LIB_DEBUG(mod, "in lib_mod_event");
+  LIB_DEBUG(mod, "out lib_mod_event");
+  return 0;
+}
+
+/******************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_signal(struct mod* mod)
+{
+  LIB_DEBUG(mod, "in lib_mod_signal");
+  LIB_DEBUG(mod, "out lib_mod_signal");
+  return 0;
+}
+
+/******************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_end(struct mod* mod)
+{
+  return 0;
+}
+
+/******************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_set_param(struct mod* mod, char* name, char* value)
+{
+  return 0;
+}
+
+/******************************************************************************/
+struct mod* EXPORT_CC
+mod_init(void)
+{
+  struct mod* mod;
+
+  mod = (struct mod*)g_malloc(sizeof(struct mod), 1);
+  mod->size = sizeof(struct mod);
+  mod->handle = (long)mod;
+  mod->mod_connect = lib_mod_connect;
+  mod->mod_start = lib_mod_start;
+  mod->mod_event = lib_mod_event;
+  mod->mod_signal = lib_mod_signal;
+  mod->mod_end = lib_mod_end;
+  mod->mod_set_param = lib_mod_set_param;
+  return mod;
+}
+
+/******************************************************************************/
+int EXPORT_CC
+mod_exit(struct mod* mod)
+{
+  if (mod == 0)
+  {
+    return 0;
+  }
+  g_free(mod);
+  return 0;
+}
