4b0ce3ac7cbfcb0758be3fc3078de1cdc9b07905
jsorg71 <jsorg71>
2008-05-06 09:30:34 +0000

    correct and add some path defines

--- common/file_loc.h
+++ common/file_loc.h
@@ -23,6 +23,10 @@
 #if !defined(FILE_LOC_H)
 #define FILE_LOC_H
 
+#if !defined(XRDP_CFG_PATH)
+#define XRDP_CFG_PATH "/etc/xrdp"
+#endif
+
 #if !defined(XRDP_CFG_FILE)
 #define XRDP_CFG_FILE "/etc/xrdp/xrdp.ini"
 #endif
@@ -31,12 +35,20 @@
 #define XRDP_KEY_FILE "/etc/xrdp/rsakeys.ini"
 #endif
 
+#if !defined(XRDP_KEYMAP_PATH)
+#define XRDP_KEYMAP_FILE "/etc/xrdp/km-%4.4x.ini"
+#endif
+
 #if !defined(XRDP_PID_FILE)
 #define XRDP_PID_FILE "/var/run/xrdp.pid"
 #endif
 
-#if !defined(XRDP_KEYMAP_PATH)
-#define XRDP_KEYMAP_FILE "/etc/km-%4.4x.ini"
+#if !defined(XRDP_SBIN_PATH)
+#define XRDP_SBIN_PATH "/usr/local/sbin"
+#endif
+
+#if !defined(XRDP_SHARE_PATH)
+#define XRDP_SHARE_PATH "/usr/local/share/xrdp"
 #endif
 
 #endif
