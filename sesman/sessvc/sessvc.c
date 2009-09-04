8a8b57153d165c48a3d6eed4e944dc37a8841cde
jsorg71 <jsorg71>
2009-09-04 19:34:24 +0000

    added chansrv_cleanup

--- sesman/sessvc/sessvc.c
+++ sesman/sessvc/sessvc.c
@@ -56,12 +56,12 @@ chansrv_cleanup(int pid)
 {
   char text[256];
 
-  g_snprintf(text, 255, "xrdp_chansrv_%8.8x_main_term", pid);
+  g_snprintf(text, 255, "/tmp/xrdp_chansrv_%8.8x_main_term", pid);
   if (g_file_exist(text))
   {
     g_file_delete(text);
   }
-  g_snprintf(text, 255, "xrdp_chansrv_%8.8x_thread_done", pid);
+  g_snprintf(text, 255, "/tmp/xrdp_chansrv_%8.8x_thread_done", pid);
   if (g_file_exist(text))
   {
     g_file_delete(text);
