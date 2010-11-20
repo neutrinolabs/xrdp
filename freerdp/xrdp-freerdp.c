565d86d2823b982a95a7023dc70edf76523803fe
Jay Sorg <jay.sorg@gmail.com>
2010-11-19 20:01:02 -0800

    started work on freerdp wrapper

---
+++ freerdp/xrdp-freerdp.c
@@ -0,0 +1,114 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   xrdp: A Remote Desktop Protocol server.
+   Copyright (C) Jay Sorg 2010
+
+   freerdp wrapper
+
+*/
+
+#include "xrdp-freerdp.h"
+
+/*****************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_start(struct mod* mod, int w, int h, int bpp)
+{
+  LIB_DEBUG(mod, "in lib_mod_start");
+  mod->width = w;
+  mod->height = h;
+  mod->bpp = bpp;
+  LIB_DEBUG(mod, "out lib_mod_start");
+  return 0;
+}
+
+/******************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_connect(struct mod* mod)
+{
+  LIB_DEBUG(mod, "in lib_mod_connect");
+  LIB_DEBUG(mod, "out lib_mod_connect");
+  return 0;
+}
+
+/******************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_event(struct mod* mod, int msg, long param1, long param2,
+              long param3, long param4)
+{
+  LIB_DEBUG(mod, "in lib_mod_event");
+  LIB_DEBUG(mod, "out lib_mod_event");
+  return 0;
+}
+
+/******************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_signal(struct mod* mod)
+{
+  LIB_DEBUG(mod, "in lib_mod_signal");
+  LIB_DEBUG(mod, "out lib_mod_signal");
+  return 0;
+}
+
+/******************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_end(struct mod* mod)
+{
+  return 0;
+}
+
+/******************************************************************************/
+/* return error */
+int DEFAULT_CC
+lib_mod_set_param(struct mod* mod, char* name, char* value)
+{
+  return 0;
+}
+
+/******************************************************************************/
+struct mod* EXPORT_CC
+mod_init(void)
+{
+  struct mod* mod;
+
+  mod = (struct mod*)g_malloc(sizeof(struct mod), 1);
+  mod->size = sizeof(struct mod);
+  mod->version = CURRENT_MOD_VER;
+  mod->handle = (long)mod;
+  mod->mod_connect = lib_mod_connect;
+  mod->mod_start = lib_mod_start;
+  mod->mod_event = lib_mod_event;
+  mod->mod_signal = lib_mod_signal;
+  mod->mod_end = lib_mod_end;
+  mod->mod_set_param = lib_mod_set_param;
+  return mod;
+}
+
+/******************************************************************************/
+int EXPORT_CC
+mod_exit(struct mod* mod)
+{
+  if (mod == 0)
+  {
+    return 0;
+  }
+  g_free(mod);
+  return 0;
+}
