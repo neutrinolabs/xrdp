eab09af9d436f241f778c675ed22b3aca0e1000d
norrarvid <norrarvid@gmail.com>
2011-09-09 11:12:39 +0200

    Color and bitmap convert fix for 15 bpp

--- freerdp/xrdp-color.c
+++ freerdp/xrdp-color.c
@@ -186,6 +186,10 @@ convert_bitmap(int in_bpp, int out_bpp, char* bmpdata,
   if ((in_bpp == 32) && (out_bpp == 32))
   {
     return bmpdata;
+  }    
+  if ((in_bpp == 15) && (out_bpp == 15))
+  {
+    return bmpdata;
   }
   g_writeln("convert_bitmap: error unknown conversion from %d to %d",
             in_bpp, out_bpp);
@@ -262,6 +266,10 @@ convert_color(int in_bpp, int out_bpp, int in_color, int* palette)
     return in_color;
   }
   if ((in_bpp == 32) && (out_bpp == 32))
+  {
+    return in_color;
+  }  
+  if ((in_bpp == 15) && (out_bpp == 15))
   {
     return in_color;
   }
