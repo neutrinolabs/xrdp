84adf7a45c16b2b4704c84ae765100c02e6fc55c
Jay Sorg <jay.sorg@gmail.com>
2012-01-26 00:40:32 -0600

    added nx directory

---
+++ tests/nx/client.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+xhost +
+
+if ! [ -d .nx ]
+then
+  mkdir .nx
+fi
+
+export LD_LIBRARY_PATH=$PWD
+
+./nxproxy -S nx/nx,session=session,id=jay,root=.nx,connect=127.0.0.1:10,delta=1,stream=1,data=1
+
