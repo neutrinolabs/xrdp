4ff502aa4be7cefb86c2259d8743f0c8f94dedf5
Jay Sorg <jay.sorg@gmail.com>
2012-02-06 00:02:00 -0800

    fix init error

--- common/os_calls.c
+++ common/os_calls.c
@@ -93,7 +93,7 @@ g_init(const char* app_name)
   setlocale(LC_CTYPE, "");
   if (app_name != 0)
   {
-    if (app_name[0] == 0)
+    if (app_name[0] != 0)
     {
       snprintf(g_temp_base, sizeof(g_temp_base), "/tmp/%s-XXXXXX", app_name);
       if (mkdtemp(g_temp_base) == 0)
