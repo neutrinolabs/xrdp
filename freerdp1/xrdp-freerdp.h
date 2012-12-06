9d541ebf1af2f39a0d6ea13e2fa95fee2fe76efc
LawrenceK <github@lklyne.co.uk>
2012-12-06 08:56:08 +0000

    Chnages due to the refctoring of freerdp, mainly the rdp settings object. Also use FREERDP_LIBS in the make file for xrdplib to handle the shared libraries not being in a system directyory (private build?)

--- freerdp1/xrdp-freerdp.h
+++ freerdp1/xrdp-freerdp.h
@@ -30,7 +30,7 @@
 #include <freerdp/rail.h>
 #include <freerdp/rail/rail.h>
 #include <freerdp/codec/bitmap.h>
-#include <freerdp/utils/memory.h>
+//#include <freerdp/utils/memory.h>
 //#include "/home/jay/git/jsorg71/staging/include/freerdp/freerdp.h"
 
 struct bitmap_item
