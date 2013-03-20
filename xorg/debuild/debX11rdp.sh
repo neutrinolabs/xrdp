9239b6e87ded6af7aafa4c82e270ba130f830620
Gustavo Homem <gustavo@angulosolido.pt>
2013-03-20 21:13:44 +0000

    prepare to package X11rdp on a single .deb

---
+++ xorg/debuild/debX11rdp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# receives version, release number and source directory as arguments
+
+VERSION=$1
+RELEASE=$2
+SRCDIR=$3
+PKGDEST=$4
+
+PACKDIR=x11rdp-files
+DESTDIR=$PACKDIR/opt
+NAME=x11rdp
+
+sed -i -e  "s/DUMMYVERINFO/$VERSION-$RELEASE/"  $PACKDIR/DEBIAN/control
+sed -i -e  "s/DUMMYDIRINFO/$SRCDIR/"  $PACKDIR/DEBIAN/postinst
+
+mkdir -p $DESTDIR
+cp -Rf $SRCDIR $DESTDIR
+dpkg-deb --build $PACKDIR $PKGDEST/${NAME}_$VERSION-$RELEASE.deb
+
+# revert to initial state
+rm -rf $DESTDIR
+sed -i -e  "s/$VERSION-$RELEASE/DUMMYVERINFO/"  $PACKDIR/DEBIAN/control
+sed -i -e  "s/$SRCDIR/DUMMYDIRINFO/"  $PACKDIR/DEBIAN/postinst
