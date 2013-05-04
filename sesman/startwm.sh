2086cc25e7af16a2d4f7ad8ff8007568ad8b7484
Jay Sorg <jay.sorg@gmail.com>
2013-05-03 20:07:31 -0700

    startwm.sh should look for Xsession

--- sesman/startwm.sh
+++ sesman/startwm.sh
@@ -4,3 +4,23 @@ if [ -r /etc/default/locale ]; then
   export LANG LANGUAGE
 fi
 . /etc/X11/Xsession
+
+# debian
+if [ -r /etc/X11/Xsession ]; then
+  . /etc/X11/Xsession
+  exit 0
+fi
+
+# el
+if [ -r /etc/X11/xinit/Xsession ]; then
+  . /etc/X11/xinit/Xsession
+  exit 0
+fi
+
+# suse
+if [ -r /etc/X11/xdm/Xsession ]; then
+  . /etc/X11/xdm/Xsession
+  exit 0
+fi
+
+xterm
