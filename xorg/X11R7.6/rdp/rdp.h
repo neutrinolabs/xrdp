neutrinolabs/xrdp

commit cb1efca63704d342b3d1aba764f6971e2656a8d1

Hand-apply patches (glyph cache) from Authentic8 branch: 653869c 30f23d4 74b015d 4e51e6d 2829087

xorg/X11R7.6/rdp/rdp.h
@@ -95,6 +95,23 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define PixelDPI 100
 #define PixelToMM(_size) (((_size) * 254 + (PixelDPI) * 5) / ((PixelDPI) * 10))
 
+#define TAG_COMPOSITE     0
+#define TAG_COPYAREA      1
+#define TAG_POLYFILLRECT  2
+#define TAG_PUTIMAGE      3
+#define TAG_POLYRECTANGLE 4
+#define TAG_COPYPLANE     5
+#define TAG_POLYARC       6
+#define TAG_FILLPOLYGON   7
+#define TAG_POLYFILLARC   8
+#define TAG_IMAGETEXT8    9
+#define TAG_POLYTEXT8     10
+#define TAG_POLYTEXT16    11
+#define TAG_IMAGETEXT16   12
+#define TAG_IMAGEGLYPHBLT 13
+#define TAG_POLYGLYPHBLT  14
+#define TAG_PUSHPIXELS    15
+
 struct image_data
 {
   int width;
@@ -144,6 +161,8 @@ struct _rdpScreenInfoRec
   CopyWindowProcPtr CopyWindow;
   ClearToBackgroundProcPtr ClearToBackground;
   ScreenWakeupHandlerProcPtr WakeupHandler;
+  CreatePictureProcPtr CreatePicture;
+  DestroyPictureProcPtr DestroyPicture;
   CompositeProcPtr Composite;
   GlyphsProcPtr Glyphs;
   /* Backing store procedures */
@@ -200,6 +219,7 @@ typedef rdpWindowRec* rdpWindowPtr;
 #define RDI_IMGLY 3 /* lossy */
 #define RDI_LINE 4
 #define RDI_SCRBLT 5
+define RDI_TEXT 6
 
 struct urdp_draw_item_fill
 {
@@ -236,6 +256,13 @@ struct urdp_draw_item_scrblt
   int cy;
 };
 
+struct urdp_draw_item_text
+{
+  int opcode;
+  int fg_color;
+  struct rdp_text* rtext; /* in rdpglyph.h */
+};
+
 union urdp_draw_item
 {
   struct urdp_draw_item_fill fill;
@@ -246,7 +273,7 @@ union urdp_draw_item
 
 struct rdp_draw_item
 {
-  int type;
+  int type; /* RDI_FILL, RDI_IMGLL, ... */
   int flags;
   struct rdp_draw_item* prev;
   struct rdp_draw_item* next;
@@ -327,6 +354,8 @@ void
 hexdump(unsigned char *p, unsigned int len);
 void
 RegionAroundSegs(RegionPtr reg, xSegment* segs, int nseg);
+int
+get_crc(char* data, int data_bytes);
 
 /* rdpdraw.c */
 Bool
@@ -343,7 +372,7 @@ int
 draw_item_pack(PixmapPtr pix, rdpPixmapRec* priv);
 int
 draw_item_add_img_region(rdpPixmapRec* priv, RegionPtr reg, int opcode,
-                         int type);
+                         int type, int code);
 int
 draw_item_add_fill_region(rdpPixmapRec* priv, RegionPtr reg, int color,
                           int opcode);
@@ -355,6 +384,9 @@ int
 draw_item_add_srcblt_region(rdpPixmapRec* priv, RegionPtr reg,
                             int srcx, int srcy, int dstx, int dsty,
                             int cx, int cy);
+int
+draw_item_add_text_region(rdpPixmapRec* priv, RegionPtr reg, int color,
+                          int opcode, struct rdp_text* rtext);
 
 PixmapPtr
 rdpCreatePixmap(ScreenPtr pScreen, int width, int height, int depth,
@@ -413,6 +445,10 @@ rdpDisplayCursor(ScreenPtr pScreen, CursorPtr pCursor);
 void
 rdpRecolorCursor(ScreenPtr pScreen, CursorPtr pCursor,
                  Bool displayed);
+int
+rdpCreatePicture(PicturePtr pPicture);
+void
+rdpDestroyPicture(PicturePtr pPicture);
 void
 rdpComposite(CARD8 op, PicturePtr pSrc, PicturePtr pMask, PicturePtr pDst,
              INT16 xSrc, INT16 ySrc, INT16 xMask, INT16 yMask, INT16 xDst,
@@ -523,6 +559,19 @@ int
 rdpup_check_dirty(PixmapPtr pDirtyPixmap, rdpPixmapRec* pDirtyPriv);
 int
 rdpup_check_dirty_screen(rdpPixmapRec* pDirtyPriv);
+int
+rdpup_add_char(int font, int charactor, short x, short y, int cx, int cy,
+               char* bmpdata, int bmpdata_bytes);
+int
+rdpup_add_char_alpha(int font, int charactor, short x, short y, int cx, int cy,
+                     char* bmpdata, int bmpdata_bytes);
+int
+rdpup_draw_text(int font, int flags, int mixmode,
+                short clip_left, short clip_top,
+                short clip_right, short clip_bottom,
+                short box_left, short box_top,
+                short box_right, short box_bottom, short x, short y,
+                char* data, int data_bytes);
 
 void
 rdpScheduleDeferredUpdate(void);

libxrdp/libxrdpinc.h
@@ -40,13 +40,15 @@ struct xrdp_pen
   int color;
 };
 
+/* 2.2.2.2.1.2.5.1 Cache Glyph Data (TS_CACHE_GLYPH_DATA) */
 struct xrdp_font_char
 {
-  int offset;
-  int baseline;
-  int width;
-  int height;
+  int offset;    /* x */
+  int baseline;  /* y */
+  int width;     /* cx */
+  int height;    /* cy */
   int incby;
+  int bpp;
   char* data;
 };
 

libxrdp/xrdp_orders.c
@@ -2015,15 +2015,25 @@ xrdp_orders_send_font(struct xrdp_orders *self,
     int order_flags = 0;
     int datasize = 0;
     int len = 0;
+    int flags;
 
-    datasize = FONT_DATASIZE(font_char);
+    if (font_char->bpp == 8) /* alpha font */
+    {
+        datasize = ((font_char->width + 3) & ~3) * font_char->height;
+        flags = 8 | 0x4000;
+    }
+    else
+    {
+        datasize = FONT_DATASIZE(font_char);
+        flags = 8;
+    }
     xrdp_orders_check(self, datasize + 18);
     self->order_count++;
     order_flags = RDP_ORDER_STANDARD | RDP_ORDER_SECONDARY;
     out_uint8(self->out_s, order_flags);
     len = (datasize + 12) - 7; /* length after type minus 7 */
     out_uint16_le(self->out_s, len);
-    out_uint16_le(self->out_s, 8); /* flags */
+    out_uint16_le(self->out_s, flags);
     out_uint8(self->out_s, RDP_ORDER_FONTCACHE); /* type */
     out_uint8(self->out_s, font_index);
     out_uint8(self->out_s, 1); /* num of chars */

xorg/X11R7.6/rdp/Makefile
@@ -13,7 +13,7 @@ rdpPolylines.o rdpPolySegment.o rdpFillSpans.o rdpSetSpans.o \
 rdpCopyPlane.o rdpPolyPoint.o rdpPolyArc.o rdpFillPolygon.o \
 rdpPolyFillArc.o rdpPolyText8.o rdpPolyText16.o \
 rdpImageText8.o rdpImageText16.o rdpImageGlyphBlt.o rdpPolyGlyphBlt.o \
-rdpPushPixels.o rdpxv.o \
+rdpPushPixels.o rdpxv.o rdpglyph.o \
 miinitext.o \
 fbcmap_mi.o
 

xorg/X11R7.6/rdp/rdpCopyArea.c
@@ -460,6 +460,8 @@ rdpCopyArea(DrawablePtr pSrc, DrawablePtr pDst, GCPtr pGC,
         }
     }
 
+    LLOGLN(10, ("rdpCopyArea: fallback"));
+    
     /* do original call */
     rv = rdpCopyAreaOrg(pSrc, pDst, pGC, srcx, srcy, w, h, dstx, dsty);
 
@@ -537,7 +539,7 @@ rdpCopyArea(DrawablePtr pSrc, DrawablePtr pDst, GCPtr pGC,
             box.x2 = box.x1 + w;
             box.y2 = box.y1 + h;
             RegionInit(&reg1, &box, 0);
-            draw_item_add_img_region(pDirtyPriv, &reg1, GXcopy, dirty_type);
+            draw_item_add_img_region(pDirtyPriv, &reg1, GXcopy, dirty_type, TAG_COPYAREA);
             RegionUninit(&reg1);
         }
         else if (got_id)
@@ -561,7 +563,7 @@ rdpCopyArea(DrawablePtr pSrc, DrawablePtr pDst, GCPtr pGC,
                 box.y2 = box.y1 + h;
                 RegionInit(&box_reg, &box, 0);
                 RegionIntersect(&clip_reg, &clip_reg, &box_reg);
-                draw_item_add_img_region(pDirtyPriv, &clip_reg, GXcopy, dirty_type);
+                draw_item_add_img_region(pDirtyPriv, &clip_reg, GXcopy, dirty_type, TAG_COPYAREA);
                 RegionUninit(&box_reg);
             }
             else if (got_id)

xorg/X11R7.6/rdp/rdpCopyPlane.c
@@ -168,7 +168,7 @@ rdpCopyPlane(DrawablePtr pSrc, DrawablePtr pDst,
             box.x2 = box.x1 + w;
             box.y2 = box.y1 + h;
             RegionInit(&reg1, &box, 0);
-            draw_item_add_img_region(pDirtyPriv, &reg1, GXcopy, dirty_type);
+            draw_item_add_img_region(pDirtyPriv, &reg1, GXcopy, dirty_type, TAG_COPYPLANE);
             RegionUninit(&reg1);
         }
         else if (got_id)
@@ -194,7 +194,7 @@ rdpCopyPlane(DrawablePtr pSrc, DrawablePtr pDst,
                 RegionInit(&reg2, NullBox, 0);
                 RegionCopy(&reg2, &clip_reg);
                 RegionIntersect(&reg1, &reg1, &reg2);
-                draw_item_add_img_region(pDirtyPriv, &reg1, GXcopy, dirty_type);
+                draw_item_add_img_region(pDirtyPriv, &reg1, GXcopy, dirty_type, TAG_COPYPLANE);
                 RegionUninit(&reg1);
                 RegionUninit(&reg2);
             }

xorg/X11R7.6/rdp/rdpFillPolygon.c
@@ -203,7 +203,7 @@ rdpFillPolygon(DrawablePtr pDrawable, GCPtr pGC,
         if (dirty_type != 0)
         {
             RegionInit(&reg1, &box, 0);
-            draw_item_add_img_region(pDirtyPriv, &reg1, GXcopy, dirty_type);
+            draw_item_add_img_region(pDirtyPriv, &reg1, GXcopy, dirty_type, TAG_FILLPOLYGON);
             RegionUninit(&reg1);
         }
         else if (got_id)
@@ -223,7 +223,7 @@ rdpFillPolygon(DrawablePtr pDrawable, GCPtr pGC,
         {
             if (dirty_type != 0)
             {
-                draw_item_add_img_region(pDirtyPriv, &clip_reg, GXcopy, dirty_type);
+                draw_item_add_img_region(pDirtyPriv, &clip_reg, GXcopy, dirty_type, TAG_FILLPOLYGON);
             }
             else if (got_id)
             {

xorg/X11R7.6/rdp/rdpImageGlyphBlt.c
@@ -169,7 +169,7 @@ rdpImageGlyphBlt(DrawablePtr pDrawable, GCPtr pGC,
         if (dirty_type != 0)
         {
             RegionInit(&reg1, &box, 0);
-            draw_item_add_img_region(pDirtyPriv, &reg1, GXcopy, dirty_type);
+            draw_item_add_img_region(pDirtyPriv, &reg1, GXcopy, dirty_type, TAG_IMAGEGLYPHBLT);
             RegionUninit(&reg1);
         }
         else if (got_id)
@@ -189,7 +189,7 @@ rdpImageGlyphBlt(DrawablePtr pDrawable, GCPtr pGC,
         {
             if (dirty_type != 0)
             {
-                draw_item_add_img_region(pDirtyPriv, &reg, GXcopy, dirty_type);
+                draw_item_add_img_region(pDirtyPriv, &reg, GXcopy, dirty_type, TAG_IMAGEGLYPHBLT);
             }
             else if (got_id)
             {

commit 363d35fa21ee164dffd0edc31586bf5d2e59e8ea

Fix typo introduced in cb1efca6

xorg/X11R7.6/rdp/rdp.h
@@ -219,7 +219,7 @@ typedef rdpWindowRec* rdpWindowPtr;
 #define RDI_IMGLY 3 /* lossy */
 #define RDI_LINE 4
 #define RDI_SCRBLT 5
-define RDI_TEXT 6
+#define RDI_TEXT 6
 
 struct urdp_draw_item_fill
 {