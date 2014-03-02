374633e63f35170ad25b4841a4214273560a671b
Jay Sorg <jay.sorg@gmail.com>
2014-03-01 23:26:40 -0800

    common: no logic change, update comments

--- common/rail.h
+++ common/rail.h
@@ -1,7 +1,7 @@
 /**
  * xrdp: A Remote Desktop Protocol server.
  *
- * Copyright (C) Jay Sorg 2012
+ * Copyright (C) Jay Sorg 2012-2014
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
