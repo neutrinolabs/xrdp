b30e5ca14a444ae6af85f9f4db6d6229f5a36ed2
Jay Sorg <jay.sorg@gmail.com>
2014-05-19 12:45:49 -0700

    neutrinordp: minor changes, coding style, log level

--- neutrinordp/xrdp-neutrinordp.c
+++ neutrinordp/xrdp-neutrinordp.c
@@ -27,7 +27,7 @@
 #ifdef XRDP_DEBUG
 #define LOG_LEVEL 99
 #else
-#define LOG_LEVEL 0
+#define LOG_LEVEL 1
 #endif
 
 #define LLOG(_level, _args) \
@@ -195,12 +195,13 @@ lxrdp_event(struct mod *mod, int msg, long param1, long param2,
     switch (msg)
     {
         case 15: /* key down */
-            // Before we handle the first character we synchronize 
-            // capslock and numlock. 
-	    // We collect the state during the first synchronize ( see msg 17)
-            if(!mod->bool_keyBoardSynced)
+            /* Before we handle the first character we synchronize
+               capslock and numlock. */
+            /* We collect the state during the first synchronize
+               ( see msg 17) */
+            if (!mod->bool_keyBoardSynced)
             {
-                LLOGLN(0, ("Additional Sync event handled : %d",mod->keyBoardLockInfo));
+                LLOGLN(11, ("Additional Sync event handled : %d", mod->keyBoardLockInfo));
                 mod->inst->input->SynchronizeEvent(mod->inst->input, mod->keyBoardLockInfo);
                 mod->bool_keyBoardSynced = 1;
             }
@@ -209,16 +210,17 @@ lxrdp_event(struct mod *mod, int msg, long param1, long param2,
         case 16: /* key up */
             mod->inst->input->KeyboardEvent(mod->inst->input, param4, param3);
             break;
-        case 17: /*Synchronize*/       
+        case 17: /* Synchronize */
             LLOGLN(11, ("Synchronized event handled : %d",param1));
-            // In some situations the Synchronize event come to early. 
-            // Therefore we store this information and use it when we 
-            // receive the first keyboard event
-            // Without this fix numlock and capslock can come out of sync.	    
+            /* In some situations the Synchronize event come to early.
+               Therefore we store this information and use it when we
+               receive the first keyboard event
+               Without this fix numlock and capslock can come
+               out of sync. */
             mod->inst->input->SynchronizeEvent(mod->inst->input, param1);
-            if(!mod->bool_keyBoardSynced)
+            if (!mod->bool_keyBoardSynced)
             {
-                mod->keyBoardLockInfo = param1 ;
+                mod->keyBoardLockInfo = param1;
             }
             break;
         case 100: /* mouse move */
