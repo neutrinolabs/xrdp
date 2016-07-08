28a1a090b3463042d2c03964f42da1f0f18f2c23
Pavel Roskin <plroskin@gmail.com>
2016-07-08 01:33:29 +0000

    Use correct types for ssl and ctx fields in struct ssl_tls

--- common/ssl_calls.h
+++ common/ssl_calls.h
@@ -84,8 +84,8 @@ ssl_gen_key_xrdp1(int key_size_in_bits, char* exp, int exp_len,
 /* ssl_tls */
 struct ssl_tls
 {
-    void *ssl; /* SSL * */
-    void *ctx; /* SSL_CTX * */
+    struct ssl_st *ssl; /* SSL * */
+    struct ssl_ctx_st *ctx; /* SSL_CTX * */
     char *cert;
     char *key;
     struct trans *trans;
