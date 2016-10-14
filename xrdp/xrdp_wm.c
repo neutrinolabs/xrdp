8d431ce8b3f533dabe9aaed0a762d5e54ea14762
Ricardo Duarte <ricardoteixeiraduarte@gmail.com>
2016-10-14 17:13:05 +0100

    Move mouse before click

--- xrdp/xrdp_wm.c
+++ xrdp/xrdp_wm.c
@@ -1240,6 +1240,11 @@ xrdp_wm_mouse_click(struct xrdp_wm *self, int x, int y, int but, int down)
         {
             if (self->mm->mod->mod_event != 0)
             {
+				if (down)
+				{
+					self->mm->mod->mod_event(self->mm->mod, WM_MOUSEMOVE, x, y, 0, 0);
+				}
+
                 if (but == 1 && down)
                 {
                     self->mm->mod->mod_event(self->mm->mod, WM_LBUTTONDOWN, x, y, 0, 0);
