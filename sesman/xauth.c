1430c54c4090fba87d64df0f1256aca95d1fd789
BLINDAUER Emmanuel <e.blindauer@gmail.com>
2016-12-16 20:55:52 +0100

    - Removed tailing space - Fixed the copyright and year

--- sesman/xauth.c
+++ sesman/xauth.c
@@ -1,8 +1,7 @@
 /**
  * xrdp: A Remote Desktop Protocol server.
  *
- * Copyright (C) Jay Sorg 2004-2017
- * Copyright (C) Emmanuel Blindauer 2004-2017
+ * Copyright (C) Emmanuel Blindauer 2016
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- sesman/xauth.h
+++ sesman/xauth.h
@@ -1,7 +1,7 @@
 /**
  * xrdp: A Remote Desktop Protocol server.
  *
- * Copyright (C) Jay Sorg 2004-2013
+ * Copyright (C) Emmanuel Blindauer 2016
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,9 +18,8 @@
 
 /**
  *
- * @file xauth.c                                                                 
- * @brief XAUTHORITY handling code                                               
- * @author Emmaunel Blindauer                                                    
+ * @file xauth.c
+ * @brief XAUTHORITY handling code
  *
  */
 
