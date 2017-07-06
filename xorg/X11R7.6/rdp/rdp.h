2b9b2c11b3624d84ea055b7b5aa04cc30675d0ca
Koichiro IWAO <meta@vmeta.jp>
2017-07-06 15:11:56 +0900

    Prepare release v0.9.3

--- xorg/X11R7.6/rdp/rdp.h
+++ xorg/X11R7.6/rdp/rdp.h
@@ -76,7 +76,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 //#include "colormapst.h"
 
-#define X11RDPVER "0.9.2"
+#define X11RDPVER "0.9.3"
 
 #define PixelDPI 100
 #define PixelToMM(_size) (((_size) * 254 + (PixelDPI) * 5) / ((PixelDPI) * 10))
