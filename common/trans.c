8495cacbe20b4124bf1c0088712e38c190237a19
Ian Geiser <geiseri@geekcentral.pub>
2017-07-07 09:43:01 -0400

    make sure directory is created before we try to create a domain socket

--- common/trans.c
+++ common/trans.c
@@ -808,6 +808,8 @@ trans_listen_address(struct trans *self, char *port, const char *address)
     }
     else if (self->mode == TRANS_MODE_UNIX) /* unix socket */
     {
+        g_create_path(port);
+
         g_free(self->listen_filename);
         self->listen_filename = 0;
         g_file_delete(port);
