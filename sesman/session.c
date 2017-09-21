94ce323ec5bcee5ca4afb3e725f071f9ca6e5a57
BLINDAUER Emmanuel <e.blindauer@gmail.com>
2017-09-21 09:01:20 +0200

    Initial support for utmp/wtmp on linux

--- sesman/session.c
+++ sesman/session.c
@@ -502,6 +502,7 @@ session_start_fork(tbus data, tui8 type, struct SCP_CONNECTION *c,
                         g_getpid());
         }
 #endif
+        utmp_login(g_getpid(), display, s->username, s->client_ip);
         window_manager_pid = g_fork(); /* parent becomes X,
                              child forks wm, and waits, todo */
         if (window_manager_pid == -1)
@@ -926,8 +927,8 @@ session_kill(int pid)
         {
             /* deleting the session */
             log_message(LOG_LEVEL_INFO, "++ terminated session:  username %s, display :%d.0, session_pid %d, ip %s", tmp->item->name, tmp->item->display, tmp->item->pid, tmp->item->client_ip);
+            utmp_logout(tmp->item->pid, tmp->item->display, tmp->item->name, tmp->item->client_ip);
             g_free(tmp->item);
-
             if (prev == 0)
             {
                 /* prev does no exist, so it's the first element - so we set
