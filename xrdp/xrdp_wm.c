neutrinolabs/xrdp

commit 93d97dcb08a171b8dee04c8e504ad50f452ea098

Fix memory leak in font handler
The following sequence of events results in a memory leak as a font
is loaded twice
1) Invoke login screen
2) Try to log in to session, which fails
3) Back to login screen

Fix is to make sure font is deleted before being reloaded.

xrdp/xrdp_wm.c
@@ -577,6 +577,7 @@ xrdp_wm_init(struct xrdp_wm *self)
                      self->screen->bpp);
 
     /* Load the font */
+    xrdp_font_delete(self->default_font);
     dpi = xrdp_login_wnd_get_monitor_dpi(self);
     self->default_font = xrdp_font_create(self, dpi);
 

commit 4c8997f03347bd4d3260242ef87139b0ec61746b

Fix regression use-after-free
93d97dcb introduced a use-after-free error. The default_font for the
window manager may be used as the font for the main window manager
painter, and so deleting the default_font may cause the painter to
access deallocated memory.

At the point xrdp_wm_init() is called, the only active painter
should be the window manager one.

xrdp/xrdp_wm.c
@@ -574,8 +574,11 @@ xrdp_wm_init(struct xrdp_wm *self)
     load_xrdp_config(self->xrdp_config, self->session->xrdp_ini,
                      self->screen->bpp);
 
-    /* Load the font */
+    /* Remove a font loaded on the previous config */
     xrdp_font_delete(self->default_font);
+    self->painter->font = NULL; /* May be set to the default_font */
+
+    /* Load the font */
     dpi = xrdp_login_wnd_get_monitor_dpi(self);
     self->default_font = xrdp_font_create(self, dpi);
 