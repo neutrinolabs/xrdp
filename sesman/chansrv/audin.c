5f07c648e45cdbd5eeea127322e32dd48582f436
Koichiro IWAO <meta@vmeta.jp>
2023-02-09 00:53:49 +0900

    chansrv: add a utility function to convert wFormatTag into string

--- sesman/chansrv/audin.c
+++ sesman/chansrv/audin.c
@@ -89,6 +89,27 @@ static struct xr_wave_format_ex **g_client_formats = NULL;
 static int g_current_format = 0; /* index in g_client_formats */
 
 /*****************************************************************************/
+
+/*
+ * This can be called from sound.c because it includes audin.h.
+ */
+
+const char *
+audin_wave_format_tag_to_str(int tag)
+{
+    return
+        (tag == WAVE_FORMAT_PCM)        ? "WAVE_FORMAT_PCM" :
+        (tag == WAVE_FORMAT_ADPCM)      ? "WAVE_FORMAT_ADPCM" :
+        (tag == WAVE_FORMAT_ALAW)       ? "WAVE_FORMAT_ALAW" :
+        (tag == WAVE_FORMAT_MULAW)      ? "WAVE_FORMAT_MULAW" :
+        (tag == WAVE_FORMAT_MULAW)      ? "WAVE_FORMAT_MULAW" :
+        (tag == WAVE_FORMAT_MPEGLAYER3) ? "WAVE_FORMAT_MPEGLAYER3" :
+        (tag == WAVE_FORMAT_OPUS)       ? "WAVE_FORMAT_OPUS" :
+        (tag == WAVE_FORMAT_AAC)        ? "WAVE_FORMAT_AAC" :
+        NULL;
+}
+
+/*****************************************************************************/
 static int
 cleanup_client_formats(void)
 {
