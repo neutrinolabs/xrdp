5c242c1869c7ce701b3848340c0202c42a228f6b
firewave <firewave@users.noreply.github.com>
2023-10-20 17:20:32 +0200

    fixed `-Wunused-function` compiler warnings

--- tests/xrdp/test_xrdp_region.c
+++ tests/xrdp/test_xrdp_region.c
@@ -61,7 +61,9 @@ static pixman_box16_t test_box =
     0, 0, 100, 100
 };
 
-static pixman_box16_t *__wrap_pixman_region_extents(pixman_region16_t *region)
+// cmocka requires this to be externally linkable
+extern pixman_box16_t *__wrap_pixman_region_extents(pixman_region16_t *region);
+pixman_box16_t *__wrap_pixman_region_extents(pixman_region16_t *region)
 {
     check_expected_ptr(region);
     return mock_ptr_type(pixman_box16_t *);
@@ -107,7 +109,9 @@ static void test_xrdp_region_get_bounds__happy_path(void **state)
     g_free(rect);
 }
 
-static pixman_bool_t __wrap_pixman_region_not_empty(pixman_region16_t *region)
+// cmocka requires this to be externally linkable
+extern pixman_bool_t __wrap_pixman_region_not_empty(pixman_region16_t *region);
+pixman_bool_t __wrap_pixman_region_not_empty(pixman_region16_t *region)
 {
     check_expected_ptr(region);
     return mock_type(pixman_bool_t);
