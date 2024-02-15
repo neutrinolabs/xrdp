9ad0d6abb617514885f78efbe7217854899a5ecb
Koichiro Iwao <meta@vmeta.jp>
2024-02-15 13:21:17 +0900

    Just log Image RemoteFX codec

--- libxrdp/xrdp_caps.c
+++ libxrdp/xrdp_caps.c
@@ -603,6 +603,12 @@ xrdp_caps_process_codecs(struct xrdp_rdp *self, struct stream *s, int len)
             g_memcpy(self->client_info.h264_prop, s->p, i1);
             self->client_info.h264_prop_len = i1;
         }
+        /* other known codec but not supported yet */
+        else if (g_memcmp(codec_guid, XR_CODEC_GUID_IMAGE_REMOTEFX, 16) == 0)
+        {
+            LOG(LOG_LEVEL_INFO, "xrdp_caps_process_codecs: Image RemoteFX(%s), codec id [%d], properties len [%d]",
+                codec_guid_str, codec_id, codec_properties_length);
+        }
         else
         {
             LOG(LOG_LEVEL_WARNING, "xrdp_caps_process_codecs: unknown(%s), codec id [%d], properties len [%d]",
