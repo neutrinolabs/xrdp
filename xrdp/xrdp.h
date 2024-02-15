bc70a86de6139f6effe4f1e20be39188e9bde172
matt335672 <30179339+matt335672@users.noreply.github.com>
2024-02-15 16:20:04 +0000

    Rework xrdp to support new module resize interface
    This commit compiles.

--- xrdp/xrdp.h
+++ xrdp/xrdp.h
@@ -591,7 +591,10 @@ server_draw_text(struct xrdp_mod *mod, int font,
                  int box_right, int box_bottom,
                  int x, int y, char *data, int data_len);
 int
-server_reset(struct xrdp_mod *mod, int width, int height, int bpp);
+client_monitor_resize(struct xrdp_mod *mod, int width, int height,
+                      int num_monitors, const struct monitor_info *monitors);
+int
+server_monitor_resize_done(struct xrdp_mod *mod);
 int
 is_channel_allowed(struct xrdp_wm *wm, int channel_id);
 int
