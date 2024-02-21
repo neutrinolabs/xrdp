84a747c42dce07312a778fb7cc2b07ae56df3233
Koichiro Iwao <meta@vmeta.jp>
2024-02-21 19:09:34 +0900

    Bump copyright year and make easier to bump
    (cherry picked from commit ae249c675542092b4487ce040cb6fc06d43f1e87)

--- sesman/sesman.c
+++ sesman/sesman.c
@@ -567,8 +567,9 @@ print_version(void)
 {
     g_writeln("xrdp-sesman %s", PACKAGE_VERSION);
     g_writeln("  The xrdp session manager");
-    g_writeln("  Copyright (C) 2004-2020 Jay Sorg, "
-              "Neutrino Labs, and all contributors.");
+    g_writeln("  Copyright (C) 2004-%d Jay Sorg, "
+              "Neutrino Labs, and all contributors.",
+              VERSION_YEAR);
     g_writeln("  See https://github.com/neutrinolabs/xrdp for more information.");
     g_writeln("%s", "");
 
