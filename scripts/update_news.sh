dad7766afccd1e8f7e66c1f25c49873a9e0e5574
Koichiro Iwao <meta@vmeta.jp>
2024-05-10 17:36:32 +0900

    Turn off cache explicitly when fetching NEWS from wiki

--- scripts/update_news.sh
+++ scripts/update_news.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-curl https://raw.githubusercontent.com/wiki/neutrinolabs/xrdp/NEWS-v0.10.md > \
+curl -s -H 'Cache-Control: no-cache' \
+    https://raw.githubusercontent.com/wiki/neutrinolabs/xrdp/NEWS-v0.10.md > \
     $(git rev-parse --show-superproject-working-tree --show-toplevel | head -1)/NEWS.md
