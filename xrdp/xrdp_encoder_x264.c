099575056eb38106fe2032c1741ae3d08c1e893d
Jay Sorg <jay.sorg@gmail.com>
2024-05-19 22:20:33 -0700

    fix for odd width, height
    (cherry picked from commit 485e64ee76afc919489a6e4591536f6110f4f5d6)

--- xrdp/xrdp_encoder_x264.c
+++ xrdp/xrdp_encoder_x264.c
@@ -138,8 +138,8 @@ xrdp_encoder_x264_encode(void *handle, int session, int left, int top,
             //x264_param_default_preset(&(xe->x264_params), "superfast", "zerolatency");
             x264_param_default_preset(&(xe->x264_params), "ultrafast", "zerolatency");
             xe->x264_params.i_threads = 1;
-            xe->x264_params.i_width = width;
-            xe->x264_params.i_height = height;
+            xe->x264_params.i_width = (width + 15) & ~15;
+            xe->x264_params.i_height = (height + 15) & ~15;
             xe->x264_params.i_fps_num = 24;
             xe->x264_params.i_fps_den = 1;
             //xe->x264_params.b_cabac = 1;
