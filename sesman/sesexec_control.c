b4ddca14e1d7a5e3d7c55742db562c9587cf4216
matt335672 <30179339+matt335672@users.noreply.github.com>
2025-02-05 10:25:08 +0000

    Coverity CID 468118
    (cherry picked from commit 8769481136e38c3e1bae7d221f4b4904822b8dc4)

--- sesman/sesexec_control.c
+++ sesman/sesexec_control.c
@@ -165,7 +165,11 @@ sesexec_start(struct pre_session_item *psi)
                  * in the environment */
                 char buff[64];
                 g_snprintf(buff, sizeof(buff), "%d", sck[1]);
-                g_setenv("EICP_FD", buff, 1);
+                if (g_setenv("EICP_FD", buff, 1) < 0)
+                {
+                    LOG(LOG_LEVEL_ERROR, "Can't set EICP_FD [%s]",
+                        g_get_strerror());
+                }
 
                 /* [Development] Log all file descriptors not marked cloexec
                  * other than stdin, stdout, stderr, and the EICP fd in sck[1].
