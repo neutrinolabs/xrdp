e534a4b216d427b4ad2dd0b4e507a6d6157be5dd
matt335672 <30179339+matt335672@users.noreply.github.com>
2025-04-17 12:02:18 +0100

    Fix Coverity CID 468122
    Missing break statement in a switch intoduced by commit
    54acca43cfe85d60f8a8963fe7ac84cd293d5be2
    
    The results of this are benign, as the extra code which is run is
    unlikely to do anything.
    
    (cherry picked from commit c198b321a2272bd72279d0b2944c7c2daff0fdeb)

--- xrdp/xrdp_wm.c
+++ xrdp/xrdp_wm.c
@@ -2059,6 +2059,7 @@ callback(intptr_t id, int msg, intptr_t param1, intptr_t param2,
             xrdp_mm_suppress_output(wm->mm, param1,
                                     LOWORD(param2), HIWORD(param2),
                                     LOWORD(param3), HIWORD(param3));
+            break;
         case 0x555a:
             // "yeah, up_and_running"
             xrdp_mm_up_and_running(wm->mm);
