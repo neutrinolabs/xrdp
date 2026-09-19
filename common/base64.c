0bc00b8342539633ab508e27edc3b81857f8c833
Idan Freiberg <3459028+speidy@users.noreply.github.com>
2026-09-19 16:16:34 +0300

    Preserve original copyright attribution in existing files

--- common/base64.c
+++ common/base64.c
@@ -1,6 +1,5 @@
 /**
  * Copyright (C) 2022 Matt Burt, all xrdp contributors
- * Copyright (C) Idan Freiberg 2026
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
